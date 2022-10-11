63ab7d2551106b2d4ccc2e2ae5bcf740b3907e4f
Ahmad Aulia Noorhakim <aaulia@gmail.com>
2022-10-12 02:03:21 +0700

    Fixing MovieActivity package name in manifest

--- app/src/main/kotlin/aaulia/compose/movie/app/MovieActivity.kt
+++ app/src/main/kotlin/aaulia/compose/movie/app/MovieActivity.kt
@@ -1,6 +1,5 @@
 package aaulia.compose.movie.app
 
-import aaulia.compose.movie.ui.theme.MovieAppTheme
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
