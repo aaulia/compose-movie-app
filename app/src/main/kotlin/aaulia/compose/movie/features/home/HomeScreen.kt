9645d3c522b653e8fc57558722cb3d18534fa25c
Ahmad Aulia Noorhakim <uli.noorhakim@bhinneka.com>
2022-10-12 15:40:38 +0700

    Add dummy movie list

--- app/src/main/kotlin/aaulia/compose/movie/features/home/HomeScreen.kt
+++ app/src/main/kotlin/aaulia/compose/movie/features/home/HomeScreen.kt
@@ -1,14 +1,18 @@
 package aaulia.compose.movie.features.home
 
 import aaulia.compose.movie.ui.theme.MovieAppTheme
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.material.*
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
@@ -55,9 +59,41 @@ fun HomeScreen(
             navController = navController,
             startDestination = HomeNavItem.Default.route
         ) {
-            composable(HomeNavItem.Playing.route) { }
-            composable(HomeNavItem.Popular.route) { }
-            composable(HomeNavItem.Nearing.route) { }
+            composable(HomeNavItem.Playing.route) { MovieList() }
+            composable(HomeNavItem.Popular.route) { MovieList() }
+            composable(HomeNavItem.Nearing.route) { MovieList() }
+        }
+    }
+}
+
+@Composable
+fun MovieList() {
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(2),
+        contentPadding = PaddingValues(8.dp),
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        items(20) { index ->
+            MovieItem(index)
+        }
+    }
+}
+
+@Composable
+fun MovieItem(index: Int) {
+    Surface(
+        modifier = Modifier
+            .safeContentPadding()
+            .height(192.dp),
+        shape = MaterialTheme.shapes.medium,
+        color = MaterialTheme.colors.secondary
+    ) {
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = "$index")
         }
     }
 }
