d4df3c1d8b63020fc9cd447cd44e860321c76bd8
Ahmad Aulia Noorhakim <aaulia@gmail.com>
2022-10-14 01:17:31 +0700

    Implement Staggered List, add placeholders and show poster image in list

---
+++ app/src/main/kotlin/aaulia/compose/movie/features/list/ListItem.kt
@@ -0,0 +1,72 @@
+package aaulia.compose.movie.features.list
+
+import aaulia.compose.movie.R
+import aaulia.compose.movie.data.model.MovieItem
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
+
+@Composable
+fun ListItem(
+    movie: MovieItem,
+    onClick: (Int) -> Unit = { }
+) {
+    Column(
+        modifier = Modifier
+            .clip(RoundedCornerShape(8.dp))
+            .clickable { onClick(movie.id) }
+    ) {
+
+        if (movie.poster.isNotEmpty()) {
+            AsyncImage(
+                model =
+                ImageRequest.Builder(LocalContext.current)
+                    .data(movie.poster)
+                    .crossfade(true)
+                    .build(),
+                contentScale = ContentScale.FillWidth,
+                contentDescription = movie.title,
+                placeholder = painterResource(id = R.drawable.placeholder_loading),
+                error = painterResource(id = R.drawable.placeholder_failure),
+                modifier = Modifier.fillMaxWidth()
+            )
+        } else {
+            Image(
+                painter = painterResource(id = R.drawable.placeholder_default),
+                contentDescription = movie.title,
+                contentScale = ContentScale.FillWidth,
+                modifier = Modifier.fillMaxWidth()
+            )
+        }
+
+        Text(
+            text = movie.title,
+            color = MaterialTheme.colors.onPrimary,
+            fontFamily = FontFamily.SansSerif,
+            fontSize = 12.sp,
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .background(color = MaterialTheme.colors.primaryVariant)
+                .padding(horizontal = 16.dp, vertical = 8.dp)
+                .fillMaxWidth()
+        )
+    }
+}
--- app/src/main/kotlin/aaulia/compose/movie/features/list/ListScreen.kt
+++ app/src/main/kotlin/aaulia/compose/movie/features/list/ListScreen.kt
@@ -1,18 +1,11 @@
 package aaulia.compose.movie.features.list
 
-import aaulia.compose.movie.data.repository.TMDBRepository
-import aaulia.compose.movie.ui.theme.MovieAppTheme
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
+import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.lifecycle.viewmodel.initializer
@@ -26,6 +19,7 @@ enum class MovieType {
 }
 
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun ListScreen(
     movieType: MovieType,
@@ -38,59 +32,17 @@ fun ListScreen(
         })
 ) {
     val movies = viewModel.movieFlow.collectAsLazyPagingItems()
-    LazyVerticalGrid(
-        columns = GridCells.Fixed(2),
+
+    LazyVerticalStaggeredGrid(
+        columns = StaggeredGridCells.Fixed(2),
         contentPadding = PaddingValues(8.dp),
         horizontalArrangement = Arrangement.spacedBy(8.dp),
         verticalArrangement = Arrangement.spacedBy(8.dp)
     ) {
         items(
-            count = movies.itemCount,
+            count = movies.itemCount
         ) { index ->
-            movies[index]?.let { movie -> ListItem(movie.id, onClick) }
+            movies[index]?.let { movie -> ListItem(movie, onClick) }
         }
-    }
-}
-
-
-@Preview(showBackground = true)
-@Composable
-fun ListPreview() {
-    MovieAppTheme {
-        ListScreen(MovieType.PLAYING)
-    }
-}
-
-
-@Composable
-fun ListItem(
-    index: Int,
-    onClick: (Int) -> Unit = { }
-) {
-    Surface(
-        modifier = Modifier
-            .safeContentPadding()
-            .height(192.dp)
-            .clickable {
-                onClick(index)
-            },
-        shape = MaterialTheme.shapes.medium,
-        color = MaterialTheme.colors.secondary
-    ) {
-        Column(
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Text(text = "$index")
-        }
-    }
-}
-
-
-@Preview(showBackground = true)
-@Composable
-fun ListItemPreview() {
-    MovieAppTheme {
-        ListItem(8)
     }
 }
