73e3fb7f53dac900c5abd16317ef0b6590a9f315
Ahmad Aulia Noorhakim <uli.noorhakim@bhinneka.com>
2022-10-16 09:00:54 +0700

    Simplify movie model

--- app/src/main/kotlin/aaulia/compose/movie/data/local/model/Movie.kt
+++ app/src/main/kotlin/aaulia/compose/movie/data/local/model/Movie.kt
@@ -11,48 +11,35 @@ import androidx.room.PrimaryKey
 data class Movie(
     @PrimaryKey
     val id: Int,
-    val imdbId: String,
 
     val title: String,
-    val tagline: String,
     val overview: String,
-
-    val popularity: Float,
-    val voteAverage: Float,
-    val voteCount: Int,
-
     val posterPath: String,
     val backdropPath: String,
 
-    val homepage: String
+    val tagline: String,
 )
 
 fun MovieSimple.toMovie() =
     Movie(
         id = id,
-        imdbId = "",
+
         title = title,
-        tagline = "",
         overview = overview,
-        popularity = popularity,
-        voteAverage = voteAverage,
-        voteCount = voteCount,
         posterPath = fullPosterPath,
         backdropPath = fullBackdropPath,
-        homepage = ""
+
+        tagline = "",
     )
 
 fun MovieDetail.toMovie() =
     Movie(
         id = id,
-        imdbId = imdbId,
+
         title = title,
-        tagline = tagline,
         overview = overview,
-        popularity = popularity,
-        voteAverage = voteAverage,
-        voteCount = voteCount,
         posterPath = fullPosterPath,
         backdropPath = fullBackdropPath,
-        homepage = homepage
+
+        tagline = tagline
     )
--- app/src/main/kotlin/aaulia/compose/movie/data/remote/model/MovieData.kt
+++ app/src/main/kotlin/aaulia/compose/movie/data/remote/model/MovieData.kt
@@ -16,17 +16,8 @@ data class MovieSimple(
     val id: Int,
     val title: String,
     val overview: String,
-    val popularity: Float = 0.0f,
-
-    @SerialName("vote_average")
-    val voteAverage: Float = 0.0f,
-
-    @SerialName("vote_count")
-    val voteCount: Int = 0,
-
     @SerialName("poster_path")
     override val posterPath: String = "",
-
     @SerialName("backdrop_path")
     override val backdropPath: String = ""
 ) : MovieImages
@@ -37,38 +28,16 @@ data class MovieDetail(
     val id: Int,
     val title: String,
     val overview: String,
-    val popularity: Float = 0.0f,
-
-    @SerialName("vote_average")
-    val voteAverage: Float = 0.0f,
-
-    @SerialName("vote_count")
-    val voteCount: Int = 0,
-
     @SerialName("poster_path")
     override val posterPath: String = "",
-
     @SerialName("backdrop_path")
     override val backdropPath: String = "",
 
-    /**
-     * detail info
-     */
-
     @SerialName("imdb_id")
     val imdbId: String = "",
-
     val tagline: String = "",
     val homepage: String = "",
-    val genres: List<MovieGenre> = emptyList()
 ) : MovieImages
-
-
-@Serializable
-data class MovieGenre(
-    val id: Int,
-    val name: String
-)
 
 
 val MovieImages.fullPosterPath: String
